e0a04bafa09d0ff72c9464d226b28b6c9956ef8d
Tuuli Martikainen <tuuli.martikainen@edu.karelia.fi>
2021-12-29 15:16:52 +0200

    3.3-3.6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     {
@@ -41,7 +42,65 @@ let persons = [
   app.get('/api/persons', (req, res) => {
     res.json(persons)
   })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+  app.post('/api/persons', (request, response) => {
+        const body = request.body
+        console.log(body)
+
   
+    if (!body.name) {
+        return response.status(400).json({ 
+          error: 'name is missing' 
+        })
+      } else if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number is missing' 
+        })
+      }
+    
+    if (persons.find(i => i.name === body.name)) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+    }
+
+      const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number        
+      }
+    
+      persons = persons.concat(person)
+    
+    
+    response.json(person)
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
