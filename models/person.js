f2dff01df70220695bf65485dc1c4afda64ca784
Tuuli Martikainen <tuuli.martikainen@edu.karelia.fi>
2022-01-06 15:08:36 +0200

    last changes

--- models/person.js
+++ models/person.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
--- mongo.js
+++ mongo.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 const personSchema = new mongoose.Schema({
